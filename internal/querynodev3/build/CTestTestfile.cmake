3ec8291369c5aecdbbe4c8404ca63404b2da76c9
yah01 <yang.cen@zilliz.com>
2022-11-14 14:14:17 +0800

    Add QueryNodeV3
    Signed-off-by: yah01 <yang.cen@zilliz.com>

---
+++ internal/querynodev3/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yah01/src/milvus/internal/querynodev3
+# Build directory: /home/yah01/src/milvus/internal/querynodev3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
